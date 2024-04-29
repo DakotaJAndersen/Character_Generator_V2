f17f57dad8bba2182122543790a36dd2f835ecba
Dakota Andersen <anhemphill@csumb.edu>
2024-04-28 19:23:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/charactergenerator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.charactergenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
