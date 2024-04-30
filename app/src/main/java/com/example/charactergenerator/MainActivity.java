d058c83bd75708b53962be9544b0d9771ae32ad1
Dakota Andersen <anhemphill@csumb.edu>
2024-04-29 18:27:18 -0700

    Made the Saved Characters button respond to tap/click with a toast informing user that the page it leads to is not yet implemented.

--- app/src/main/java/com/example/charactergenerator/MainActivity.java
+++ app/src/main/java/com/example/charactergenerator/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.charactergenerator;
 
+import static com.example.charactergenerator.databinding.ActivityMainBinding.inflate;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.charactergenerator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.generatorSavedCharactersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Saved Character Screen not yet implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
