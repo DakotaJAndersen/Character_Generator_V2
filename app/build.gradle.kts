47904b249ef9e0488c8cba3da6d27cb1f9a17301
Dakota Andersen <anhemphill@csumb.edu>
2024-04-29 17:50:44 -0700

    Added viewBinding=true to build.gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,10 @@ android {
     namespace = "com.example.charactergenerator"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.charactergenerator"
         minSdk = 29
