c3e850ac40425b55363548129dfe1b376833b234
Dakota Andersen <anhemphill@csumb.edu>
2024-05-03 19:14:32 -0700

    Got default users working, added preset hair types.

--- app/src/main/java/com/Database/CharacterGeneratorDatabase.java
+++ app/src/main/java/com/Database/CharacterGeneratorDatabase.java
@@ -1,6 +1,7 @@
 package com.Database;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -10,15 +11,16 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.Database.Entities.HairType;
 import com.Database.Entities.User;
+import com.example.charactergenerator.MainActivity;
 
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {HairType.class, User.class}, version = 2, exportSchema = false)
+@Database(entities = {HairType.class, User.class}, version = 7, exportSchema = false)
 public abstract class CharacterGeneratorDatabase extends RoomDatabase {
 
-    public static final String USER_TABLE = "user_table";
+    public static final String USER_TABLE = "userTable";
     private static final String DATABASE_NAME = "CharacterGeneratorDatabase";
     public static final String hairTypeTable = "hairTypeTable";
     private static volatile CharacterGeneratorDatabase INSTANCE;
@@ -33,8 +35,6 @@ public abstract class CharacterGeneratorDatabase extends RoomDatabase {
                                     DATABASE_NAME
                                     )
                             .fallbackToDestructiveMigration()
-                            //this is commented out due to studio yelling at me and I can't
-                            //figure out why?  It doesn't let me declare addDefaultValues.
                             .addCallback(addDefaultValues)
                             .build();
 
@@ -48,13 +48,33 @@ public abstract class CharacterGeneratorDatabase extends RoomDatabase {
             @Override
             public void onCreate(@NonNull SupportSQLiteDatabase db){
                 super.onCreate(db);
-                //TODO: add databaseWriteExecutor.execute(() -> {...}
+                Log.i(MainActivity.TAG, "Database Created!");
+                databaseWriteExecutor.execute(() -> {
+                    UserDAO userdao = INSTANCE.userDAO();
+                    //dao.deleteAll();
+
+                    //Default Users
+                    User admin = new User("admin1", "admin1");
+                    admin.setAdmin(true);
+                    userdao.insert(admin);
+                    User testUser1 = new User("testuser1", "testuser1");
+                    userdao.insert(testUser1);
+
+                    //Preset Hair Types
+                    HairTypeDAO hairtypedao = INSTANCE.hairTypeDAO();
+                    HairType curly = new HairType("curly");
+                    hairtypedao.insert(curly);
+                    HairType wavy = new HairType("wavy");
+                    hairtypedao.insert(wavy);
+                    HairType straight = new HairType("straight");
+                    hairtypedao.insert(straight);
+                    HairType fancy = new HairType("fancy");
+                    hairtypedao.insert(fancy);
+                    HairType casualHair = new HairType("casual");
+                    hairtypedao.insert(casualHair);
+                        });
             }
         };
-
-
-
-
 
     public abstract HairTypeDAO hairTypeDAO();
 
